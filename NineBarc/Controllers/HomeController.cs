f1598a9610549b6e1b997a774768186d65e0b38d
mmelber <melber@gmx.de>
2020-02-26 15:19:58 +0100

    initial commit

---
+++ NineBarc/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NineBarc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.Message = "Dashboard";
+
+            return View();
+        }
+
+        public ActionResult MyVault()
+        {
+            ViewBag.Message = "My vault";
+
+            return View();
+        }
+        public ActionResult MyReceivers()
+        {
+            ViewBag.Message = "My Receivers";
+
+            return View();
+        }
+    }
+}
---
+++ NineBarc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NineBarc.MvcApplication" Language="C#" %>
